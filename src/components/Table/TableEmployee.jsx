137a9ba839304edf26ed8fba76c09a55db7eb1d3
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-06-11 23:07:39 +0200

    create table

--- src/components/Table/TableEmployee.jsx
+++ src/components/Table/TableEmployee.jsx
@@ -1,45 +1,136 @@
 import React, { useMemo } from "react";
-import { useTable } from "react-table";
-import Data from "../../Data/employee_data.json";
+import { useSelector } from "react-redux";
+import { usePagination, useTable } from "react-table";
+// import Data from "../../Data/employee_data.json";
 import { columns } from "./Columns";
 import "./TableEmployee.scss";
 
 const TableEmployee = () => {
-  const tableColumns = useMemo(() => columns, []);
-  const tableData = useMemo(() => [...Data], []);
-  const tableInstance = useTable({
-    columns: tableColumns,
-    data: tableData,
-  });
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
+  const data = useSelector((state) => state.employee);
 
-  console.log(tableInstance);
+  const tableColumns = useMemo(() => columns, []);
+  const tableData = useMemo(() => [...data.employeeList], []);
+
+  const tableInstance = useTable(
+    {
+      columns: tableColumns,
+      data: tableData,
+    },
+    usePagination
+  );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    page,
+    gotoPage,
+    pageCount,
+    setPageSize,
+    prepareRow,
+    state: { pageIndex, pageSize },
+  } = tableInstance;
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((header) => (
-              <th {...header.getHeaderProps()}>{header.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("cell")}</td>;
-              })}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((header) => (
+                <th {...header.getHeaderProps()}>{header.render("Header")}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="table-pagination">
+        <div>
+          <select
+            className="page-size"
+            value={pageSize}
+            onChange={(e) => setPageSize(Number(e.target.value))}
+          >
+            {[10, 25, 50, 100].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                show{pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <span>
+            Page {""}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>
+            {""}
+          </span>
+          <span>
+            | go to page{""}
+            <input
+              className="page-number"
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const pageNumber = e.target.value
+                  ? Number(e.target.value) - 1
+                  : 0;
+                gotoPage(pageNumber);
+              }}
+            />
+          </span>
+          <div className="pagination-button">
+            <button
+              onClick={() => gotoPage(0)}
+              disabled={!canPreviousPage}
+              aria-label="go to first page"
+            >
+              {"<<"}
+            </button>
+            <button
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+              aria-label="go to previous page"
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => nextPage()}
+              disabled={!canNextPage}
+              aria-label="go to next page"
+            >
+              Next
+            </button>
+            <button
+              onClick={() => gotoPage(pageCount - 1)}
+              disabled={!canNextPage}
+              aria-label="go to last page"
+            >
+              {">>"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
