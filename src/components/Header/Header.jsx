a47dd3a613680f5b95ca1cf9d2871f309e4799b0
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-06-11 23:05:15 +0200

    add link to button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import logo from "./logo.png";
 import "./Header.scss";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
-    <div className="header">
-      <img className="logo" src={logo} alt="" />
-      <span className="title">HRNET</span>
-      <span></span>
+    <div className="header" aria-label="page header">
+      <img className="logo" src={logo} alt="logo" />
+      <span className="title" aria-label="page title">
+        HRNET
+      </span>
+      {location.pathname === "/employee-list" ? (
+        <Link to="/" className="add-button">
+          <i className="fa-solid fa-user-plus"></i> <span>Add employee</span>
+        </Link>
+      ) : (
+        <Link to="/employee-list" className="add-button">
+          <i className="fa-solid fa-user"></i> <span>Employee List</span>
+        </Link>
+      )}
     </div>
   );
 };
