90d811c2d1d73a273c4d79ad8e8e22c17d250068
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-06-11 22:50:16 +0200

    create storage

---
+++ src/storage.js
@@ -0,0 +1,15 @@
+import defaultEmployeeList from "./Data/employee_data.json";
+
+const getEmployeeList = () => {
+  const data = localStorage.getItem("employee-list");
+  console.log({ data });
+  const list = data === null ? defaultEmployeeList : JSON.parse(data);
+
+  return list;
+};
+const setEmployeeList = (employeeList) => {
+  localStorage.setItem("employee-list", JSON.stringify(employeeList));
+};
+
+const StorageHelper = { getEmployeeList, setEmployeeList };
+export default StorageHelper;
