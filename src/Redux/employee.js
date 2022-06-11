fef6976bbaad99476837e9910deecf3e54ad61ef
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-06-11 22:57:36 +0200

    create employee redux

---
+++ src/Redux/employee.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import StorageHelper from "../storage";
+
+const employee = createSlice({
+  name: "add",
+  initialState: {
+    employeeList: StorageHelper.getEmployeeList(),
+  },
+
+  reducers: {
+    addEmployee: (state, action) => {
+      console.log(action.payload);
+
+      state.employeeList.unshift(action.payload);
+      StorageHelper.setEmployeeList(state.employeeList);
+    },
+  },
+});
+
+const employeeAction = employee.actions;
+const employeeReducer = employee.reducer;
+export { employeeReducer, employeeAction };
