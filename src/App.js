cea44bdb0be8b19f1159904e572038fec830649d
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-06-11 22:49:22 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import MainLayout from "./Layout/MainLayout";
+import EmployeListPage from "./Pages/EmployeListPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/employee-list" element={<EmployeListPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
