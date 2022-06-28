4117aaf36ebe744177932ea214bec30cc8819531
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-06-28 11:37:00 +0200

    add filtered list

--- src/Pages/EmployeListPage.jsx
+++ src/Pages/EmployeListPage.jsx
@@ -1,15 +1,33 @@
-import React from "react";
-import { SearchInput, TableEmployee } from "../components";
-import "./EmployeList.scss";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { SearchInput, TableEmployee } from '../components';
+import { employeeAction } from '../Redux/employee';
+import './EmployeList.scss';
 
 const EmployeListPage = () => {
+  const employeState = useSelector(state => state.employee);
+  const [filteredList, setFilteredList] = useState(employeState.employeeList);
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const list = !searchText
+      ? employeState.employeeList
+      : employeState.employeeList.filter(employe =>
+          Object.values(employe)
+            .join(' ')
+            .toLowerCase()
+            .includes(searchText.toLowerCase()),
+        );
+    setFilteredList(list);
+  }, [searchText, employeState.employeeList]);
+
   return (
     <main className="employe-list" aria-label="employee list page">
       <h1>Employee List</h1>
       <section>
-        <SearchInput />
+        <SearchInput onSearch={setSearchText} />
       </section>
-      <TableEmployee />
+      <TableEmployee tableData={filteredList} />
     </main>
   );
 };
